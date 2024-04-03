bc1bbe8ad9e0eb9152252198b8ed058bee0a608f
Anjan  Kumar Guha <66314225+Anjan26@users.noreply.github.com>
2024-04-03 22:31:09 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>
-     <div>
-       <h4>1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
-      brief explanation for each.
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="max-w-5xl w-full justify-between font-mono text-sm lg:flex">
+        <h4>1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
+          brief explanation for each.
+           </h4>
+          <h4>1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
+          brief explanation for each.
        </h4>
-      <h4>1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
-      brief explanation for each.
-   </h4>
-      <h4>1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
-      brief explanation for each.
-   </h4>
-      <h4>1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
-      brief explanation for each.
-   </h4>
-  </div>
+          <h4>1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
+          brief explanation for each.
+       </h4>
+          <h4>1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
+          brief explanation for each.
+       </h4>
+      </div>
 
-</main>
- 
+    </main>
   );
 }
