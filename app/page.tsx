612bfb308caa63feec96d53fa3ca8f3dcacc9590
Anjan  Kumar Guha <66314225+Anjan26@users.noreply.github.com>
2024-04-03 22:18:23 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
    <h4>1. From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
       brief explanation for each.
    </h4>
+     <p>From this list, rank your 5 most favourite and 5 least favourite support tasks. Provide a
+      brief explanation for each.</p>
    
 </main>
  
